ae36259dcfb0e89d9a3347d75b7da196f2b03333
mamingshuai <mamingshuai1@huawei.com>
2023-12-13 17:08:37 +0800

    Upgrade to ArkUI-X-1.0.0-Release
    Signed-off-by: mamingshuai <mamingshuai1@huawei.com>

--- uitest/core/driver.cpp
+++ uitest/core/driver.cpp
@@ -90,7 +90,7 @@ void Driver::PressBack()
     HILOG_DEBUG("Driver::PressBack called");
     auto uicontent = GetUIContent();
     CHECK_NULL_VOID(uicontent);
-    uicontent->Finish();
+    uicontent->ProcessBackPressed();
 }
 
 void Driver::DelayMs(int dur)
@@ -520,9 +520,11 @@ On* On::Text(const string& text, MatchPattern pattern)
     if (pattern >= MatchPattern::EQUALS && pattern <= MatchPattern::ENDS_WITH) {
         this->text = std::make_shared<string>(text);
         this->pattern_ = pattern;
+        this->isEnter = true;
         HILOG_DEBUG("On::Text success");
+    } else {
+        this->isEnter = false;
     }
-    this->isEnter = true;
     return this;
 }
 
